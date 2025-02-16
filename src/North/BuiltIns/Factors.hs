e4c3284ee806d41b558aa9dc0903963d07d620e7
Michael "Gilli" Gilliland <mjg.py3@gmail.com>
2025-02-16 18:53:31 -0500

    Add more basic stack manipulation builtins

--- src/North/BuiltIns/Factors.hs
+++ src/North/BuiltIns/Factors.hs
@@ -12,6 +12,7 @@ import North.Eval.DescribedFactor
 import North.Types
 import North.Values
 import Data.Bifunctor (second)
+import Data.List ((!?))
 
 builtInFactors :: [(T.Text, DescribedFactor FactorDefinition)]
 builtInFactors = [
@@ -25,6 +26,71 @@ builtInFactors = [
               case pop envState of
                 Left err -> (envState, Left err)
                 Right (envState', v) -> (push v $ push v envState', Right Unit)
+          }
+   ),
+   ("drop", DescribedFactor {
+            factorDescription = "Drop top element"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TVar 'a']
+            , stackOut=[]
+            }
+          , factorDefinition = \envState ->
+              case pop envState of
+                Left err -> (envState, Left err)
+                Right (envState', _) -> (envState', Right Unit)
+          }
+   ),
+   ("rot", DescribedFactor {
+            factorDescription = "Rotate the top three stack entries"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TVar 'a', TVar 'b', TVar 'c']
+            , stackOut=[TVar 'c', TVar 'b', TVar 'a']
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              (envState'', b) <- pop envState'
+              (envState''', c) <- pop envState''
+              pure $ push c $ push b $ push a $ envState'''
+          }
+   ),
+   ("nip", DescribedFactor {
+            factorDescription = "Removes the item directly below the top"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TVar 'a', TVar 'b']
+            , stackOut=[TVar 'a']
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              (envState'', _) <- pop envState'
+              pure $ push a $ envState''
+          }
+   ),
+   ("tuck", DescribedFactor {
+            factorDescription = "Duplicates the item directly below the top on top"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TVar 'a', TVar 'b']
+            , stackOut=[TVar 'b', TVar 'a', TVar 'b']
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              (envState'', b) <- pop envState'
+              pure $ push b $ push a $ push b $ envState''
+          }
+   ),
+   ("pick", DescribedFactor {
+            factorDescription = "Duplicates an item by index. The top indicates the index of the item below (zero-based) to duplicate"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TNum, TVar 'a', TVar 'b', TVar 'c']
+            , stackOut=[TVar 'd']
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState'@EnvState {stack=st}, a) <- pop envState
+              case a of
+                NInt i ->
+                  case st !? fromIntegral i of
+                    Nothing -> Left StackUnderflow
+                    Just v -> pure $ push v envState'
+                nonNumber -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
           }
    ),
    ("+", DescribedFactor {
@@ -63,6 +129,53 @@ builtInFactors = [
                 (NInt _, nonNumber) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
                 (NFloat _, nonNumber) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
                 (nonNumber, _) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
+          }
+   ),
+   ("-", DescribedFactor {
+            factorDescription = "Subtract top from next"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TNum]
+            , stackOut=[TNum, TNum]
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              (envState'', b) <- pop envState'
+              case (a, b) of
+                (NInt n1, NInt n2) -> pure $ push (NInt $ n2 - n1) envState''
+                (NFloat n1, NFloat n2) -> pure $ push (NFloat $ n2 - n1) envState''
+                (NInt n1, NFloat n2) -> pure $ push (NFloat $ n2 - fromIntegral n1) envState''
+                (NFloat n1, NInt n2) -> pure $ push (NFloat $ fromIntegral n2 - n1) envState''
+                (NInt _, nonNumber) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
+                (NFloat _, nonNumber) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
+                (nonNumber, _) -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
+          }
+   ),
+   ("incr", DescribedFactor {
+            factorDescription = "Increment top number"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TNum]
+            , stackOut=[TNum]
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              case a of
+                NInt n1 -> pure $ push (NInt $ n1 + 1) envState'
+                NFloat n1 -> pure $ push (NFloat $ n1 + 1) envState'
+                nonNumber -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
+          }
+   ),
+   ("decr", DescribedFactor {
+            factorDescription = "Decrement top number"
+          , factorSloppySignature = SloppySignature {
+              stackIn=[TNum]
+            , stackOut=[TNum]
+            }
+          , factorDefinition = \envState -> either (second Left . (envState, )) ((, Right Unit)) $ do
+              (envState', a) <- pop envState
+              case a of
+                NInt n1 -> pure $ push (NInt $ n1 - 1) envState'
+                NFloat n1 -> pure $ push (NFloat $ n1 - 1) envState'
+                nonNumber -> Left $ TNum `TypeExpectedButGot'` (typeOf nonNumber, nonNumber)
           }
    ),
    ("=", DescribedFactor {
